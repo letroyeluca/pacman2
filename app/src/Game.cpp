a25c05709f124025e6e2ae748c49c1a5e4c5eb86
letroyeluca <luca.letroye@student.uantwerpen.be>
2025-11-27 11:07:42 +0100

    added sfml window

--- app/src/Game.cpp
+++ app/src/Game.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "Game.h"
+#include <SFML/Graphics.hpp>
+
+void Game::run(int width, int height){
+
+    // initialiseer de window
+    sf::RenderWindow window(sf::VideoMode(width, height), "PACMAN");
+    //zet de framerate limiet
+    window.setFramerateLimit(60);
+
+    //window loop
+    while (window.isOpen()){
+        //verwerk mogelijke events
+        sf::Event event;
+        while(window.pollEvent(event)){
+            //sluiten van window
+            if(event.type == sf::Event::Closed){
+                window.close();
+            }
+            //resizing
+            if(event.type == sf::Event::Resized){
+                //statemanager verwerkt resize
+            }
+            // statemanager
+        }
+
+        //rendering logica
+        window.clear();
+        //statemamanager Render
+        window.display();
+    }
+}
