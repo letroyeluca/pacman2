a6e01f32692295c266529295c6be1a820ce9a714
GitHub Action (clang-format) <action@github.com>
2025-12-06 13:45:14 +0000

    style: auto-format C++ code with clang-format-18

--- logic/src/utils/StopWatch.cpp
+++ logic/src/utils/StopWatch.cpp
@@ -3,20 +3,18 @@
 //
 
 #include "logic/utils/StopWatch.h"
-namespace logic{
-    Stopwatch& Stopwatch::getInstance() {
-        static Stopwatch instance;
-        return instance;
-    }
-
-    Stopwatch::Stopwatch() {
-        m_lastTime = std::chrono::steady_clock::now();
-    }
-
-    float Stopwatch::tick() {
-        const auto now = std::chrono::steady_clock::now();
-        std::chrono::duration<float> deltaTime = now - m_lastTime;
-        m_lastTime = now;
-        return deltaTime.count();
-    }
+namespace logic {
+Stopwatch &Stopwatch::getInstance() {
+  static Stopwatch instance;
+  return instance;
 }
+
+Stopwatch::Stopwatch() { m_lastTime = std::chrono::steady_clock::now(); }
+
+float Stopwatch::tick() {
+  const auto now = std::chrono::steady_clock::now();
+  std::chrono::duration<float> deltaTime = now - m_lastTime;
+  m_lastTime = now;
+  return deltaTime.count();
+}
+} // namespace logic
